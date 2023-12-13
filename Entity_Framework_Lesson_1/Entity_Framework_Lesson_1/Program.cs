f8036d5f8d6c1ceb1128d9c8ae10a20c3e57c32d
Miroslav Pomelov <merosmir@mail.ru>
2023-12-13 20:16:23 +0200

    Initial commit

---
+++ Entity_Framework_Lesson_1/Entity_Framework_Lesson_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Entity_Framework_Lesson_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
