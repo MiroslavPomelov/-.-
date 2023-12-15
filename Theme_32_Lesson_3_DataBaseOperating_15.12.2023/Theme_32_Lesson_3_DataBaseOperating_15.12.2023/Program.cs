8c80fac9af6d5d33360bc9da0db8282ef31060e2
Miroslav Pomelov <merosmir@mail.ru>
2023-12-15 21:21:34 +0200

    BD_operations15.12.2023

---
+++ Theme_32_Lesson_3_DataBaseOperating_15.12.2023/Theme_32_Lesson_3_DataBaseOperating_15.12.2023/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Theme_32_Lesson_3_DataBaseOperating_15._12._2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
